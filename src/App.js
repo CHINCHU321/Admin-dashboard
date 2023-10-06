a854efbee06c37657c8721b55c2e4dcf158141a6
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-06 12:16:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Space } from 'antd';
 import './App.css';
+import AppFooter from './Components/AppFooter';
+import AppHeader from './Components/AppHeader';
+import PageContent from './Components/PageContent';
+import SideMenu from './Components/SideMenu';
 
 function App() {
   return (
     <div className="App">
-      
+      <AppHeader/>
+      <Space>
+        <SideMenu></SideMenu>
+        <PageContent></PageContent>
+      </Space>
+      <AppFooter/>
     </div>
   );
 }
