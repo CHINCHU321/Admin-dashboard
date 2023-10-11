77cc87038bb27a6f5ebc4771af747b0a2ebe9664
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-11 23:30:44 +0530

    Update AppHeader.jsx

--- src/Components/AppHeader/AppHeader.jsx
+++ src/Components/AppHeader/AppHeader.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import {Image, Typography} from 'antd';
+import{ BellFilled, MailOutlined} from '@ant-design/icons';
+import {Badge, Image, Space, Typography} from 'antd';
 function AppHeader() {
   return (
     <div className='AppHeader'>
         <Image
         width={55}
         src="./logo.png" alt="logo"></Image>
-        <Typography.Title></Typography.Title>
+        <Typography.Title>Dashboard</Typography.Title>
+        <Space>
+          <Badge count={10}>
+          <MailOutlined style={{fontSize:24}}/>
+          </Badge>
+          <Badge count={20}>
+          <BellFilled style={{fontSize:24}}/>
+          </Badge>
+        </Space>
     </div>
   )
 }
