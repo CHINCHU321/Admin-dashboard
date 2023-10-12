b1ed2a9c5879931ed284634c69ecd8fe484952d8
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-12 20:11:05 +0530

    commit

--- src/Components/AppFooter/AppFooter.jsx
+++ src/Components/AppFooter/AppFooter.jsx
@@ -1,10 +1,13 @@
+import { Typography } from 'antd'
 import React from 'react'
 
 function AppFooter() {
   return (
     <div className='AppFooter'>
-      AppFooter
-    </div>
+      <Typography.Link href='tel:+123456789'>+123456789</Typography.Link>
+      <Typography.Link href='htttps://www.gooogle.com ' target={"_blank"}>Privacy Policy</Typography.Link>
+      <Typography.Link href='htttps://www.gooogle.com ' target={"_blank"}>Terms of use</Typography.Link>
+</div>
   )
 }
 
