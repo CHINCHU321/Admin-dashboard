0e122a49f1535fa305686423558e23dc74cdbcc8
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-13 11:19:31 +0530

    commit

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,56 @@
 import React from 'react'
-import { Card, Space, Statistic, Typography } from 'antd'
+import { Card, Space, Statistic, Table, Typography } from 'antd'
 import{ ShoppingCartOutlined, ShoppingOutlined,UserOutlined, DollarCircleOutlined } from '@ant-design/icons';
 function Dashboard() {
   return (
     <div>
        <Typography.Title level={4}>Dashboard</Typography.Title>
         <Space direction='horizontal'>
-          <DashboardCard icon={<ShoppingCartOutlined/>} title={'Orders'} value={12345} />
-          <DashboardCard icon={<ShoppingOutlined/>} title={'Inventory'} value={12345} />
-          <DashboardCard icon={<UserOutlined/>} title={'Customers'} value={12345} />
-          <DashboardCard icon={<DollarCircleOutlined/>} title={'Revenue'} value={12345} />
 
+          <DashboardCard icon={<ShoppingCartOutlined
+          style={{
+            color:"green",
+            backgroundColor:"rgba(0,255,0,0.25)",
+            borderRadius: 20,
+            fontSize:24,
+            padding:8
+           }}/>} 
+          title={'Orders'} 
+          value={12345} />
+
+          <DashboardCard
+           icon={<ShoppingOutlined  style={{
+            color:"blue",
+            backgroundColor:"rgba(0,0,255,0.25)",
+            borderRadius: 20,
+            fontSize:24,
+            padding:8
+           }}/>} 
+          title={'Inventory'} value={12345} />
+
+          <DashboardCard icon={<UserOutlined
+          style={{
+            color:"purple",
+            backgroundColor:"rgba(0,255,255,0.25)",
+            borderRadius: 20,
+            fontSize:24,
+            padding:8
+           }}/>} 
+          title={'Customers'} value={12345} />
+
+          <DashboardCard icon={<DollarCircleOutlined
+          style={{
+            color:"red",
+            backgroundColor:"rgba(255,0,0,0.25)",
+            borderRadius: 20,
+            fontSize:24,
+            padding:8
+           }}/>} 
+          title={'Revenue'} value={12345} />
+
+        </Space>
+        <Space>
+          <RecentOrders/>
         </Space>
    </div>
   )
@@ -29,5 +69,24 @@ function DashboardCard({ title,value,icon}){
     </Card>
   )
 }
+function RecentOrders(){
+  return(
+<Table
+  columns={[
+  {
+    title:'title',
+    dataIndex:'title',
+  },
+  {
+    title:'Quantity',
+    dataIndex:'quantity',
+  },
+  {
+    title:'price',
+    dataIndex:'discountPrice',
+  }
+]}
+  ></Table>
 
+  ) }
 export default Dashboard
