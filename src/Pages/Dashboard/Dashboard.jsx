1552b92ddd2591f0462c3c9fecca377db0153ea2
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-13 09:41:39 +0530

    Update Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import { Card, Space, Statistic, Typography } from 'antd'
-import{ ShoppingCartOutlined} from '@ant-design/icons';
+import{ ShoppingCartOutlined, ShoppingOutlined,UserOutlined, DollarCircleOutlined } from '@ant-design/icons';
 function Dashboard() {
   return (
     <div>
        <Typography.Title level={4}>Dashboard</Typography.Title>
         <Space direction='horizontal'>
-          <DashboardCard  title={'Orders'} value={12345} />
-          <DashboardCard  title={'Invetory'} value={12345} />
-          <DashboardCard  title={'Customers'} value={12345} />
-          <DashboardCard  title={'Revenue'} value={12345} />
+          <DashboardCard icon={<ShoppingCartOutlined/>} title={'Orders'} value={12345} />
+          <DashboardCard icon={<ShoppingOutlined/>} title={'Inventory'} value={12345} />
+          <DashboardCard icon={<UserOutlined/>} title={'Customers'} value={12345} />
+          <DashboardCard icon={<DollarCircleOutlined/>} title={'Revenue'} value={12345} />
 
         </Space>
    </div>
   )
 }
-function DashboardCard({ title,value}){
+function DashboardCard({ title,value,icon}){
   return(
     <Card>
       <Space direction='horizontal'>
           <Card>
             <Space direction='horizontal'>
-              <ShoppingCartOutlined/>
-              <Statistic title={title} value={value}/>
+            {icon}
+            <Statistic title={title} value={value}/>
             </Space>
           </Card>
         </Space>
