a731c870edca19829b85f91bf01db94b2e7980a5
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-15 23:32:33 +0530

    Update Inventory.jsx

--- src/Pages/Inventory/Inventory.jsx
+++ src/Pages/Inventory/Inventory.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
-import { Typography } from 'antd'
+import React, { useState,useEffect } from 'react'
+import { Table, Typography, Space } from 'antd'
+import { getInventory } from '../../API'
 
 function Inventory() {
+  const [loading, setLoading] = useState(false)
+  const [dataSource, setDataSource] = useState([])
+  useEffect(() => {
+    setLoading(true)
+    getInventory().then(res=>{
+      setDataSource(res.productsSpace)
+    })
+  }, [])
+  
   return (
-    <div>
+    <Space size={20}>
        <Typography.Title level={4}>Inventory</Typography.Title>
-   </div>
+      <Table>
+        columns={[
+          {
+            title:'Title',
+            dataIndex:'title',
+          },
+          {
+            title:'Price',
+            dataIndex:'price',
+          }, {
+            title:'Rating',
+            dataIndex:'rating',
+          }, {
+            title:'Stock',
+            dataIndex:'stock',
+          },
+          {
+            title:'Thumbnail',
+            dataIndex:'thambnail',
+          },
+          {
+            title:'Brand',
+            dataIndex:'brand',
+          }, {
+            title:'Category',
+            dataIndex:'category',
+          }
+        ]}
+        dataSource={dataSource}
+      </Table>
+   </Space>
   )
 }
 
