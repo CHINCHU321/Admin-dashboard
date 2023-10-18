896a59aad60032b679f2e317689239bd7658e463
CHINCHU321 <chinchubabuofficial@gmail.com>
2023-10-18 19:44:08 +0530

    Update AppHeader.jsx

--- src/Components/AppHeader/AppHeader.jsx
+++ src/Components/AppHeader/AppHeader.jsx
@@ -21,10 +21,10 @@ function AppHeader() {
   return (
     <div className="AppHeader">
       <Image
-        width={40}
-        src="https://yt3.ggpht.com/ytc/AMLnZu83ghQ28n1SqADR-RbI2BGYTrqqThAtJbfv9jcq=s176-c-k-c0x00ffffff-no-rj"
+        width={50}
+        src="logo.png"
       ></Image>
-      <Typography.Title>Aamir's Dashboard</Typography.Title>
+      <Typography.Title>Dashboard</Typography.Title>
       <Space>
         <Badge count={comments.length} dot>
           <MailOutlined
